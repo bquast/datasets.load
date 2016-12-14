1641271e450eb01d6969abfaec03e98d3064ae79
Bastiaan Quast <bquast@gmail.com>
2016-12-14 13:08:35 +0100

    fix promptDatasets

--- R/promptDatasets.R
+++ R/promptDatasets.R
@@ -5,15 +5,17 @@
 promptDatasets <- function(package = NULL, lib.loc = NULL, all=TRUE, drop.defaults=FALSE) {
 
   # return dataset info
-  dtsts <- getDatasetInfo(package, lib.loc, all, drop.defaults)
+  dtsts <- datasets(package, lib.loc, all, drop.defaults)
 
   # prompt which dataset to load
   choice <- menu(dtsts[,3])
 
   # find corresponding package
-  pkg <- dtsts[choice,][1]
+  pkgs <- dtsts[choice,1]
 
   # find corresponding dataset
-  dtsts[choice,][3]
+  dtsts <- dtsts[choice,2]
+
+  return( data(list=dtsts, package=pkgs) )
 
 }
