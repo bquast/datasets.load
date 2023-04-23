7ca6c77ec8215e4e2dd5d1621d319c7e3c49f9a5
Bastiaan Quast <bquast@gmail.com>
2023-04-23 19:29:15 +0200

    test

--- tests/testthat/test-datasets.load.R
+++ tests/testthat/test-datasets.load.R
@@ -4,4 +4,5 @@ test_that("output format matches", {
   expect_equal( length(alldata()), 4)
   expect_equal( length(datasets()), 3)
   expect_equal( datasets()[1]$Package[1], 'datasets')
+  expect_equal( printDatasets()[1]$Package[1], 'datasets')
 })
